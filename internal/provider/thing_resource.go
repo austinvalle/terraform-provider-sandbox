8e70d131af27075995749d0da8a1b8d70251510b
Austin Valle <austinvalle@gmail.com>
2024-10-17 10:11:38 -0400

    data

--- internal/provider/thing_resource.go
+++ internal/provider/thing_resource.go
@@ -2,13 +2,11 @@ package provider
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var _ resource.Resource = (*thingResource)(nil)
@@ -42,28 +40,18 @@ func (r *thingResource) Schema(ctx context.Context, req resource.SchemaRequest, 
 	}
 }
 
-type thingResourceModel struct {
-	Email types.String `tfsdk:"email"`
-	Name  types.String `tfsdk:"name"`
-	Age   types.Int64  `tfsdk:"age"`
-}
-
 func (r *thingResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var data thingResourceModel
-	req.Plan.Get(ctx, &data)
-
-	// Hey there "Austin Valle"!
-	fmt.Printf("Hey there %q!", data.Name.ValueString())
+	// POST
 }
 
 func (r *thingResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	resp.State.Raw = req.Plan.Raw
+	// PUT
 }
 
 func (r *thingResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-
+	// GET
 }
 
 func (r *thingResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-
+	// DELETE
 }
