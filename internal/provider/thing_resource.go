e0b24be37dcdd0c48c634582e9c94f2a3e312c7c
Austin Valle <austinvalle@gmail.com>
2024-10-17 11:59:56 -0400

    ex

--- internal/provider/thing_resource.go
+++ internal/provider/thing_resource.go
@@ -3,19 +3,37 @@ package provider
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var _ resource.Resource = (*thingResource)(nil)
+var _ resource.ResourceWithModifyPlan = (*thingResource)(nil)
 
 func NewThingResource() resource.Resource {
 	return &thingResource{}
 }
 
 type thingResource struct{}
+
+type thingResourceModel struct {
+	ACL                 types.String `tfsdk:"acl"`
+	AccessControlPolicy types.Object `tfsdk:"access_control_policy"`
+}
+
+func (r *thingResource) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
+	var plan, state thingResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+
+	if !plan.ACL.Equal(state.ACL) && !plan.ACL.IsNull() {
+		// Set to unknown to indicate a change is coming for this attribute
+		resp.Plan.SetAttribute(ctx, path.Root("access_control_policy"), types.ObjectUnknown( /* object attribute types */ ))
+	}
+}
 
 func (r *thingResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_thing"
@@ -24,11 +42,9 @@ func (r *thingResource) Metadata(ctx context.Context, req resource.MetadataReque
 func (r *thingResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"email": schema.StringAttribute{
-				Required: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
+			"acl": schema.StringAttribute{
+				Required:      true,
+				PlanModifiers: []planmodifier.String{},
 			},
 			"name": schema.StringAttribute{
 				Required: true,
