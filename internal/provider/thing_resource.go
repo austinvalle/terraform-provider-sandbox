645f5a316cd172bcc87949b6acce44aace6ade24
Austin Valle <austinvalle@gmail.com>
2024-10-17 09:50:19 -0400

    broken commit

--- internal/provider/thing_resource.go
+++ internal/provider/thing_resource.go
@@ -2,16 +2,14 @@ package provider
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 )
 
 var _ resource.Resource = (*thingResource)(nil)
-var _ resource.ResourceWithValidateConfig = (*thingResource)(nil)
 
 func NewThingResource() resource.Resource {
 	return &thingResource{}
@@ -19,10 +17,9 @@ func NewThingResource() resource.Resource {
 
 type thingResource struct{}
 
-type thingResourceModel struct {
-	StrValue  types.String `tfsdk:"str_value"`
-	MaxLength types.Int64  `tfsdk:"max_length"`
-}
+// type thingResourceModel struct {
+// 	Name types.String `tfsdk:"name"`
+// }
 
 func (r *thingResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_thing"
@@ -31,77 +28,34 @@ func (r *thingResource) Metadata(ctx context.Context, req resource.MetadataReque
 func (r *thingResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"str_value": schema.StringAttribute{
+			"email": schema.StringAttribute{
+				Required: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"name": schema.StringAttribute{
 				Required: true,
 			},
-			"max_length": schema.Int64Attribute{
+			"age": schema.Int64Attribute{
 				Required: true,
 			},
 		},
 	}
 }
 
-func (r *thingResource) ValidateConfig(ctx context.Context, req resource.ValidateConfigRequest, resp *resource.ValidateConfigResponse) {
-	var data thingResourceModel
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// No need to validate if str_value or max_length are null/unknown
-	if data.StrValue.IsNull() || data.StrValue.IsUnknown() ||
-		data.MaxLength.IsNull() || data.MaxLength.IsUnknown() {
-		return
-	}
-
-	if len(data.StrValue.ValueString()) > int(data.MaxLength.ValueInt64()) {
-		resp.Diagnostics.AddAttributeError(
-			path.Root("str_value"),
-			"Invalid Length",
-			fmt.Sprintf("string length must be less then %d", data.MaxLength.ValueInt64()),
-		)
-	}
-}
-
 func (r *thingResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var data thingResourceModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
-}
-
-func (r *thingResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var data thingResourceModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	resp.State.Raw = req.Plan.Raw
 }
 
 func (r *thingResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var data thingResourceModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	resp.State.Raw = req.Plan.Raw
+}
 
-	if resp.Diagnostics.HasError() {
-		return
-	}
+func (r *thingResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
 
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *thingResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var data thingResourceModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
 
-	if resp.Diagnostics.HasError() {
-		return
-	}
 }
